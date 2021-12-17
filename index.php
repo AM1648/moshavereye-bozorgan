03125bccf8edc77210f857be35a4a31c578b091d
AM1648 <a.mjahromy1648@gmail.com>
2021-12-18 01:19:31 +0330

    first push to repo

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+
+$msg_lines = file("messages.txt");
+$people_contents = file_get_contents("people.json");
+$people_obj = json_decode($people_contents, TRUE);
+
+$question = $_POST["question"];
+$is_valid = (mb_substr($question, 0, 3) == "آیا") && (mb_substr($question, -1) == "?" || mb_substr($question, -1) == "؟");
+
+$en_name = $_POST["person"] ? $_POST["person"] : array_rand($people_obj);
+$fa_name = $people_obj[$en_name];
+
+$hash_val = intval(hash("crc32b", $en_name . $question), 16);
+$msg = $msg_lines[$hash_val % sizeof($msg_lines)];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,12 +25,12 @@ $fa_name = 'حافظ';
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
     <div id="title">
-        <span id="label">پرسش:</span>
+        <span id="label"><?php if ($question) echo "پرسش: " ?></span>
         <span id="question"><?php echo $question ?></span>
     </div>
     <div id="container">
         <div id="message">
-            <p><?php echo $msg ?></p>
+            <p><?php echo (!$question ? "سؤال خود را بپرس!" : (!$is_valid ? "سؤال درستی پرسیده نشده" : $msg)) ?></p>
         </div>
         <div id="person">
             <div id="person">
@@ -36,11 +46,13 @@ $fa_name = 'حافظ';
             را از
             <select name="person">
                 <?php
-                /*
-                 * Loop over people data and
-                 * enter data inside `option` tag.
-                 * E.g., <option value="hafez">حافظ</option>
-                 */
+                foreach ($people_obj as $en_name_item => $fa_name_item) {
+                    if ($en_name == $en_name_item) {
+                        echo "<option value=\"$en_name_item\" selected=\"selected\">$fa_name_item</option>";
+                    } else {
+                        echo "<option value=\"$en_name_item\">$fa_name_item</option>";
+                    }
+                }
                 ?>
             </select>
             <input type="submit" value="بپرس"/>
